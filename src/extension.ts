212b6d2961c2b93a5e47d187dfd8c93c0e36a52e
Hamza Anis <hamzaanis9514@gmail.com>
2018-08-14 04:33:53 +0500

    V 0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as http from 'http';
+import { platform } from 'os';
+import { exec, spawn } from 'child_process';
+const axios = require('axios');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-to-hastebin" is now active!');
+    console.log('VsCode To HasteBin Loaded');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -22,36 +24,55 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function shareonhastebin() {
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
     let content = readWindow();
-    if(!content){
+    if (!content) {
         return;
-    }else{
-        try {
-            uploadCode(content);
-        } catch (error) {
-            vscode.window.showWarningMessage("Share Code Hastebin: " + error.message);
-        }
+    } else {
+        uploadCode(content);
+
     }
-    console.log(content);
 }
-var options = {
-    host: "hastebin.com",
-    path: "/documents",
-    method: "POST"
-};
-export function uploadCode(code:string){
-    var request = http.request(options, function (response: http.IncomingMessage) {
-        response.setEncoding("utf8");
-    });
-    
-    request.on("error", function (e) {
-        throw e;        
-    });
-    request.write(code);
-    request.end();
+
+export function uploadCode(code: string) {
+    axios.post('https://hastebin.com/documents', code)
+        .then(function (response: any) {
+            handleLink(response.data.key);
+        })
+        .catch(function (error: any) {
+            console.log(error);
+            vscode.window.showWarningMessage("Share Code Hastebin: " + error.message);
+        });
 }
+
+export function handleLink(key: string) {
+    let url: string = "https://hastebin.com/" + key;
+    writeToClipboard(url);
+    vscode.window.showInformationMessage("URL copied to clipboard: " + url);
+}
+
+
+function writeToClipboard(url: string): void {
+    var platform: any = process ? process.platform : platform();
+    var cmd;
+    if (platform === "win32") {
+        cmd = { command: "clip", args: [] };
+    }
+    else if (platform === "darwin") {
+        cmd = { command: "pbcopy", args: [] };
+    }
+    else {
+        cmd = { command: "xclip", args: ["clipboard"] }
+    }
+    try {
+        var copyProcess = spawn(cmd.command, cmd.args);
+
+        copyProcess.stdin.write(url);
+        copyProcess.stdin.end();
+    } catch (error) {
+        vscode.window.showWarningMessage("Unable to acess clipboard" + error.message);
+    }
+}
+
 export function readWindow(): string {
     var code;
     if (!vscode.window.activeTextEditor) {
